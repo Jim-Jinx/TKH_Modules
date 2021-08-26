78384c700d2b2d78b6b8c2e6f129bfcce4ce2796
Jimmaely Valdez <61659756+Jim-Jinx@users.noreply.github.com>
2021-08-26 14:03:10 -0400

    Add files via upload

---
+++ Module_5_Programming1/coding assignments/second_project.js
@@ -0,0 +1,13 @@
+let grade = parseInt(prompt(""))
+
+if(grade >=95){
+    console.log("A+")
+} else{ 
+    (grade <= 90 >= 88) 
+}
+
+if(grade >=95){
+    console.log("A+")
+} else{ 
+    (grade <= 90 >= 88) 
+}
