de5d0066bdf50ebee8659c7d1c4bfad3ac4cedaa
Jimmaely Valdez <61659756+Jim-Jinx@users.noreply.github.com>
2021-08-27 21:19:14 -0400

    Delete index.js

--- Module_6_Programming2/projects/Rock, Paper, Scissors/index.js
+++
@@ -1,9 +0,0 @@
-// https://codepen.io/joec718/pen/JjYqdqQconsole.log('Hello World');
-let weeklyIncome = parseInt(prompt('Weekly Income:'))
-let foodCost = parseInt(prompt('Food Cost:'))
-let housingCost = parseInt(prompt('Housing Cost:'))
-let transportationCost = parseInt(prompt('Transportation Cost:'))
-let otherCost = parseInt(prompt('Other Cost:'))
-let savingsGoal = parseInt(prompt('How much do you want to save in a year?'))
-
-console.log();
